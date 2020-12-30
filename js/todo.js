1c8eccd8dacbc39ad1603cf04551379859296fe5
mo-ri-regen <moricontb@gmail.com>
2020-12-30 11:51:57 +0900

    ToDoリストアップ

---
+++ js/todo.js
@@ -0,0 +1,10 @@
+function CrelateList(){
+
+    const text=document.getElementById("text").innerText;
+
+    const list=document.getElementById("todolist");
+
+
+    //list.createElement("li");
+    //list.createTextNode(text);
+}
