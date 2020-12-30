b3b3462656c45b4b2c74545184379b1f5341679f
mo-ri-regen <moricontb@gmail.com>
2020-12-30 19:36:42 +0900

    ボタンを押したらテキストの中身を取得し,リストを作成する

--- js/todo.js
+++ js/todo.js
@@ -1,10 +1,28 @@
-function CrelateList(){
+/*
+ボタンを押すとテキストボックスに書かれた内容を取得し,
+リストを作成する.
+*/
 
-    const text=document.getElementById("text").innerText;
+const body=document.querySelector("body");
+const input=document.querySelector("input");
+//const input=document.getElementById("input"); //  こちらでもよい
+const button=document.querySelector("button");
 
-    const list=document.getElementById("todolist");
+button.addEventListener("click",function(){
 
+    //テキストデータ取得
+    const text=input.value;
+    
+    //<ul>...</ul>作成
+    const ul=document.createElement("ul");
+    body.appendChild(ul);
 
-    //list.createElement("li");
-    //list.createTextNode(text);
-}
+    //<ul>
+    //  <li>{テキストデータ}</li>
+    //</ul>
+    //を作成
+    const li=document.createElement('li');
+    li.innerText=text;
+    const list=ul.appendChild(li);
+    
+});
