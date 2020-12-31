f52a74ab0403af6f3d42b9eb90800fba79c2f49d
mo-ri-regen <moricontb@gmail.com>
2020-12-31 13:31:36 +0900

    リストをチェックボックスに変更

--- js/todo.js
+++ js/todo.js
@@ -3,26 +3,45 @@
 リストを作成する.
 */
 
-const body=document.querySelector("body");
-const input=document.querySelector("input");
+const div = document.querySelector("div");
+const input = document.querySelector("input");
 //const input=document.getElementById("input"); //  こちらでもよい
-const button=document.querySelector("button");
+const button = document.querySelector("button");
+const ul = document.createElement("ul");
 
-button.addEventListener("click",function(){
+const listArray = [];
 
-    //テキストデータ取得
-    const text=input.value;
-    
-    //<ul>...</ul>作成
-    const ul=document.createElement("ul");
-    body.appendChild(ul);
+addEventListener("load", function () {
+  //<div>
+  //    <ul>...</ul>
+  //</div>作成
+  div.appendChild(ul);
+});
 
-    //<ul>
-    //  <li>{テキストデータ}</li>
-    //</ul>
-    //を作成
-    const li=document.createElement('li');
-    li.innerText=text;
-    const list=ul.appendChild(li);
-    
+//ボタンをクリックしたとき
+button.addEventListener("click", function () {
+  //テキストデータ取得
+  const text = input.value;
+
+  //<ul>
+  //    <li id = "checkbox" type="checkbox"></li>
+  //</ul>
+  //を作成
+  //ulの子要素はliでなければならない(w3cによって定められている)
+  const li = document.createElement("li");
+  li.setAttribute("id", "checkbox");
+  li.setAttribute("type", "checkbox");
+  ul.appendChild(li);
+
+  const inputList = document.createElement("input");
+  inputList.setAttribute("id", "checkbox");
+  inputList.setAttribute("type", "checkbox");
+  inputList.value = text;
+  li.appendChild(inputList);
+
+  //labelタグを使うことでinputタグと関連付けができる
+  const label = document.createElement("label");
+  label.setAttribute("for", "checkbox");
+  label.innerText = text;
+  li.appendChild(label);
 });
