124d24b7b2d3cdd4ed5ba391947f43e09dd1fbb0
mo-ri-regen <moricontb@gmail.com>
2021-01-01 00:18:30 +0900

    スタイルシートの追加 リセットボタン追加

--- js/todo.js
+++ js/todo.js
@@ -6,23 +6,27 @@
 const div = document.querySelector("div");
 const input = document.querySelector("input");
 //const input=document.getElementById("input"); //  こちらでもよい
-const button = document.querySelector("button");
+const toDoListCreateButton = document.getElementById("toDoListCreate");
+const allClearButton = document.getElementById("allClear");
 const ul = document.createElement("ul");
-
 const listArray = [];
 
-addEventListener("load", function () {
-  //<div>
-  //    <ul>...</ul>
-  //</div>作成
-  div.appendChild(ul);
-});
+addEventListener("load", () => {});
 
+//初期化する関数
+function initial() {
+  input.value = "";
+}
 //ボタンをクリックしたとき
-button.addEventListener("click", function () {
-  //テキストデータ取得
-  const text = input.value;
-
+toDoListCreateButton.addEventListener("click", () => {
+  if (!input.value) {
+    alert("テキストを入力してください");
+    return;
+  }
+  //<ul>タグが存在しないときにタグを作成する
+  if (!document.getElementById("ul")) {
+    div.appendChild(ul);
+  }
   //<ul>
   //    <li id = "checkbox" type="checkbox"></li>
   //</ul>
@@ -36,12 +40,28 @@ button.addEventListener("click", function () {
   const inputList = document.createElement("input");
   inputList.setAttribute("id", "checkbox");
   inputList.setAttribute("type", "checkbox");
-  inputList.value = text;
+  inputList.value = input.value;
   li.appendChild(inputList);
 
   //labelタグを使うことでinputタグと関連付けができる
   const label = document.createElement("label");
   label.setAttribute("for", "checkbox");
-  label.innerText = text;
+  label.innerText = input.value;
   li.appendChild(label);
+
+  //listArray.push(li);
+  initial();
+});
+
+allClearButton.addEventListener("click", () => {
+  //div.removeChild(ul);
+
+  /*
+  listArray.forEach((element) => {
+    const parent = element.parentNode;
+    parent.removechild;
+    //element.removechild(element.lastChild);
+  });
+  */
+  initial();
 });
